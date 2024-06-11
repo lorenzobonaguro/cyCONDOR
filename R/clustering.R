3acaa8abb207f75528fcff559af7983881e79729
leidnerj <leidnerj@bn-hl-sdflex-02.dzne.de>
2024-06-11 15:15:36 +0200

    updated clustering functions including runPhenograph and runFlowSOM. 1) added feature selection and marker saving 2) updated documentation 3) changed param names

--- R/clustering.R
+++ R/clustering.R
@@ -45,115 +45,234 @@ metaclustering <- function(fcd,
 #' runPhenograph
 #'
 #' @title runPhenograph
-#' @description Run Phenograph clustering.
+#' @description Run Phenograph based clustering.
 #' @param fcd flow cytometry dataset.
-#' @param input_type data to use for the calculation of the UMAP, e.g. "pca" (suggested option).
-#' @param data_slot name of the PCA data slot to use to harmonize. If no prefix was added the, *orig*.
+#' @param input_type data to use for the calculation, e.g. "expr" or "pca" (suggested: "pca").
+#' @param data_slot data slot to use for the calculation, e.g. "orig" or "norm".
 #' @param k K value used for clustering.
-#' @param seed Seed used for the randomization steps.
-#' @param prefix Prefix for the output.
-#' @param top_PCA Number of principal components to use for the analysis.
+#' @param seed Seed used for the randomization steps. Needed for reproducability.
+#' @param prefix Prefix of the output.
+#' @param nPC Number of principal components to use for the analysis.
+#' @param markers vector of marker names to include or exclude from the calculation according to the discard parameter. See functions used_markers and measured_markers for the extraction of markers directly from the condor object
+#' @param discard LOGICAL if the markers specified should be included, "F", or excluded, "T", from the calculation. Default = F.
+#'
 #' @import Rphenograph
 #' @importFrom igraph membership
+#'
+#'
 #' @return runPhenograph
 #'
 #' @export
-runPhenograph <- function(fcd,
-                          input_type,
-                          data_slot,
-                          k,
-                          seed,
-                          prefix = NULL,
-                          top_PCA = ncol(fcd[[input_type]][[data_slot]])) {
 
+
+runPhenograph <- function (fcd,
+                           input_type,
+                           data_slot,
+                           k,
+                           seed,
+                           prefix = NULL,
+                           nPC = ncol(fcd$pca[[data_slot]]),
+                           markers = colnames(fcd$expr[[data_slot]]),
+                           discard = FALSE)
+{
   set.seed(seed)
 
-  Rphenograph_out <- Rphenoannoy::Rphenoannoy(fcd[[input_type]][[data_slot]][,1:top_PCA], k = k)
+  # see if selected markers are present in condor_object , if input_type = "expr"
+  if (input_type == "expr"){
+    for (single in markers){
+      if (!single %in% colnames(fcd[["expr"]][["orig"]])){
+        stop(paste("ERROR:",single, "not found in expr markers."))
+      }
+    }
+
+    # define markers to use
+    if (discard == FALSE){              # (discard == F -> keep specified markers (default = all))
+
+      phe_markers <- markers
+    }
+
+    else if (discard == TRUE) {       # (discard == T -> discard specified markers, error if no markers are specified)
+
+      if (length(markers) == length(colnames(fcd$expr[[data_slot]]))){
+
+        stop("ERROR: No markers specified. Specify markers to be removed or set 'discard = F'.")
+      }
+
+      else {
+
+        phe_markers <- setdiff(colnames(fcd$expr[[data_slot]]), markers)
+
+      }
+    }
+
+    #define fcd subset for tSNE calculation
+    data1 <- fcd$expr[[data_slot]][, colnames(fcd$expr[[data_slot]]) %in% phe_markers, drop = F]
+
+
+
+  }
+  if (input_type == "pca"){
+
+    #define fcd subset for tSNE calculations and get used markers of PCA analysis
+
+    data1 <- fcd$pca[[data_slot]][,1:nPC]
+    phe_markers <- used_markers(fcd,  method = "pca", data_slot = data_slot, mute = T)
+  }
+
+
+
+
+  Rphenograph_out <- Rphenoannoy::Rphenoannoy(data1, k = k)
   Rphenograph_out <- as.matrix(membership(Rphenograph_out[[2]]))
-  Rphenograph_out <- as.data.frame(matrix(ncol=1,data=Rphenograph_out,dimnames=list(rownames(fcd$expr$orig),"Phenograph")))
-
+  Rphenograph_out <- as.data.frame(matrix(ncol = 1,
+                                          data = Rphenograph_out,
+                                          dimnames = list(rownames(fcd$expr$orig), "Phenograph")))
   Rphenograph_out$Phenograph <- as.factor(Rphenograph_out$Phenograph)
-  Rphenograph_out$Description <- paste(input_type, "_",data_slot, "_k", k, sep = "")
+  Rphenograph_out$Description <- paste(input_type, "_", data_slot, "_k", k, sep = "")
 
-  phe_name <- paste("Phenograph", sub("^_", "" , paste(prefix, input_type, data_slot, "k", k, sep = "_")), sep = "_")
+  phe_name <- paste("phenograph", sub("^_", "", paste(prefix, input_type, data_slot, "k", k, sep = "_")), sep = "_")
 
-  if (top_PCA < ncol(fcd[[input_type]][[data_slot]])) {
+  if (nPC < ncol(fcd[[input_type]][[data_slot]])) {
 
-    suffix <- paste0("top", top_PCA)
+    suffix <- paste0("top", nPC)
 
     phe_name <- paste(phe_name, suffix, sep = "_")
   }
 
   fcd[["clustering"]][[phe_name]] <- Rphenograph_out
 
-  return(fcd)
+  #save used markers in "extras"-slot
+  fcd[["extras"]][["markers"]][[paste(phe_name,"markers", sep = "_")]] <- phe_markers
 
+
+  return(fcd)
 }
+
 
 #' runFlowSOM
 #'
 #' @title runFlowSOM
 #' @description Run FlowSOM based clustering.
 #' @param fcd flow cytometry dataset.
-#' @param input_type data to use for the calculation of the UMAP, e.g. "expr" or "pca".
-#' @param data_slot name of the PCA data slot to use to harmonize. If no prefix was added the, *orig*.
-#' @param num_clusters number of final clusters.
-#' @param seed Seed used for the randomization steps.
-#' @param prefix Prefic for the output.
-#' @param ret_model Logical: if the FlowSOM model should be kept for further visualization.
-#' @param top_PCA Number of principal components to use for the analysis.
-#' @return metaclustering
+#' @param input_type data to use for the calculation, e.g. "expr" or "pca".
+#' @param data_slot data slot to use for the calculation, e.g. "orig" or "norm".
+#' @param nClusters Number of final clusters.
+#' @param seed Seed used for the randomization steps. Needed for reproducability.
+#' @param prefix Prefix of the output.
+#' @param ret_model LOGICAL if the model should be saved for future projection of the data. Default = F.
+#' @param nPC Number of principal components to use for the analysis. Default = All.
+#' @param markers vector of marker names to include or exclude from the calculation according to the discard parameter. See functions used_markers and measured_markers for the extraction of markers directly from the condor object
+#' @param discard LOGICAL if the markers specified should be included, "F", or excluded, "T", from the calculation. Default = F.
+#'
+#' @import FlowSOM
+#' @import dplyr
+#'
+#' @return runFlowSOM
 #'
 #' @export
-runFlowSOM <- function(fcd,
-                       input_type,
-                       data_slot,
-                       num_clusters,
-                       seed,
-                       prefix = NULL,
-                       ret_model = FALSE,
-                       top_PCA = ncol(fcd[[input_type]][[data_slot]])) {
 
+
+
+runFlowSOM <-  function (fcd,
+                         input_type,
+                         data_slot,
+                         nClusters,
+                         seed,
+                         prefix = NULL,
+                         ret_model = FALSE,
+                         nPC = ncol(fcd[[input_type]][[data_slot]]),
+                         markers = colnames(fcd$expr[[data_slot]]),
+                         discard = FALSE)
+{
   set.seed(seed)
 
-  som <- FlowSOM::ReadInput(as.matrix(fcd[[input_type]][[data_slot]][,1:top_PCA]), transform = FALSE, scale = FALSE)
+  # see if selected markers are present in condor_object , if input_type = "expr"
+  if (input_type == "expr"){
+    for (single in markers){
+      if (!single %in% colnames(fcd[["expr"]][["orig"]])){
+        stop(paste("ERROR:",single, "not found in expr markers."))
+      }
+    }
 
-  som <- FlowSOM::BuildSOM(som, colsToUse = 1:(ncol(fcd[[input_type]][[data_slot]])))
+    # define markers to use
+    if (discard == FALSE){              # (discard == F -> keep specified markers (default = all))
 
+      som_markers <- markers
+    }
+
+    else if (discard == TRUE) {       # (discard == T -> discard specified markers, error if no markers are specified)
+
+      if (length(markers) == length(colnames(fcd$expr[[data_slot]]))){
+
+        stop("ERROR: No markers specified. Specify markers to be removed or set 'discard = F'.")
+      }
+
+      else {
+
+        som_markers <- setdiff(colnames(fcd$expr[[data_slot]]), markers)
+
+      }
+    }
+
+    #define fcd subset for FlowSOM calculation
+    data1 <- fcd$expr[[data_slot]][, colnames(fcd$expr[[data_slot]]) %in% som_markers, drop = F]
+
+
+
+  }
+  if (input_type == "pca"){
+
+    #define fcd subset for FlowSOM calculations and get used markers of PCA analysis
+
+    data1 <- fcd$pca[[data_slot]][,1:nPC]
+    som_markers <- used_markers(fcd,  method = "pca", data_slot = data_slot, mute = T)
+  }
+
+
+  som <- FlowSOM::ReadInput(as.matrix(data1),
+                            transform = FALSE,
+                            scale = FALSE)
+
+
+
+  som <- FlowSOM::BuildSOM(som, colsToUse = 1:(ncol(data1)))  # Question: colsToUse from PCA filtered object?
   som <- FlowSOM::BuildMST(som)
-
   labels_pre <- som$map$mapping[, 1]
 
-  out <- FlowSOM::metaClustering_consensus(som$map$codes, k = num_clusters, seed = seed)
+
+  out <- FlowSOM::metaClustering_consensus(som$map$codes,
+                                           k = nClusters,
+                                           seed = seed)
+
 
   labels <- out[labels_pre]
-
   labels <- as.factor(labels)
-  Description <- paste(input_type, "_",data_slot, "_k", num_clusters, sep = "")
 
-  flSOM <- data.frame("FlowSOM" = labels, "Description" = Description)
+  Description <- paste(input_type, "_", data_slot, "_k", nClusters, sep = "")
+  flSOM <- data.frame(FlowSOM = labels, Description = Description)
 
   rownames(flSOM) <- rownames(fcd$expr$orig)
 
-  SOM_name <- paste("FlowSOM", sub("^_", "" , paste(prefix, input_type, data_slot, "k", num_clusters, sep = "_")), sep = "_")
+  SOM_name <- paste("FlowSOM", sub("^_", "", paste(prefix, input_type, data_slot, "k", nClusters, sep = "_")), sep = "_")
 
-  if (top_PCA < ncol(fcd[[input_type]][[data_slot]])) {
 
-    suffix <- paste0("top", top_PCA)
+  if (nPC < ncol(fcd[[input_type]][[data_slot]])) {
 
+    suffix <- paste0("top", nPC)
     SOM_name <- paste(SOM_name, suffix, sep = "_")
   }
 
+
   if (ret_model == TRUE) {
-
     fcd[["extras"]][["FlowSOM_model"]] <- som
-
   }
+
 
   fcd[["clustering"]][[SOM_name]] <- flSOM
 
+
+  #save used markers in "extras"-slot
+  fcd[["extras"]][["markers"]][[paste(SOM_name,"markers", sep = "_")]] <- som_markers
+
   return(fcd)
-
 }
-
-
