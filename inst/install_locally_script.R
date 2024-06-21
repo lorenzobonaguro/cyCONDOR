5db00c4ebe1411b3433dc19a0e2b7066fb726a04
lorenzobonaguro <lorenzobonaguro@gmail.com>
2024-06-21 10:41:34 +0200

    Edited local installation script to be compatible with cyCONDOR v0.1.6

--- inst/install_locally_script.R
+++ inst/install_locally_script.R
@@ -1,25 +1,21 @@
-# Install condor locally
+# Install cyCONDOR locally
 
-# *NOTE*
-# To install condor on Windows or Mac you need to have a compiler installed to build the packages from source.
-# On Windows install Rtools, the version should match the version of the majour R release used
-# On Mac install xcode
-# This code was tested on R 4.3.0 and Bioconductor 3.17, nevetheless there are no expected problems with R 4.3.1 or 4.3.2 and Bioconductor 3.18
+###################################################################################################################
+# NOTE                                                                                                            #
+#                                                                                                                 #
+# To install cyCONDOR on Windows or Mac you need to have a compiler installed to build the packages from source.  #
+# On Windows install Rtools, the version should match the version of the major R release used                     #
+# On Mac install xcode                                                                                            #
+# This code was tested on R 4.3.1 and Bioconductor 3.17,                                                          #
+# nevertheless there are no expected problems with R 4.3.2 and Bioconductor 3.18                                  #
+###################################################################################################################
 
+# First we make sure Bioconductor is installed and updated
 BiocManager::install(update = T, ask = F, version = "3.17")
 
-BiocManager::install(c("flowCore", "devtools", "Rtsne", "umap", "ggplot2", "ggpubr",
-                       "vegan", "diffusionMap", "FlowSOM", "gplots", "grid", "gridExtra",
-                       "reshape2", "RColorBrewer", "scatterplot3d", "rgl", "Biobase", "Rmisc",
-                       "tidyverse", "pheatmap", "viridis", "rainbow", "destiny",
-                       "SingleCellExperiment", "slingshot", "ggrastr",
-                       "uwot", "FlowSOM", "caret", "factoextra", "harmony", "flowWorkspace",
-                       "CytoML", "Biobase", "Hmisc", "CytoDx", "DelayedMatrixStats"), version = "3.17")
+# Next we install two dependencies which are only available on GitHub
+devtools::install_github(repo = c("JinmiaoChenLab/Rphenograph", "stuchly/Rphenoannoy", "saeyslab/CytoNorm@362ac08"))
 
-install.packages("https://cran.r-project.org/src/contrib/randomForest_4.7-1.1.tar.gz", repos=NULL, type="source")
-
-devtools::install_github("JinmiaoChenLab/Rphenograph")
-
-devtools::install_github("stuchly/Rphenoannoy")
-
-devtools::install_url("https://github.com/lorenzobonaguro/cyCONDOR/releases/download/v015/cyCONDOR_0.1.5.tar.gz")
+# Finally we install cyCONDOR, here we manually provide the link to the Bioconductor repositories.
+devtools::install_url("https://github.com/lorenzobonaguro/cyCONDOR/releases/download/v015/cyCONDOR_0.1.5.tar.gz",
+                      repos = BiocManager::repositories())
